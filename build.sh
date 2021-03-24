cccdadee547a428b4cec7a850a23ae077a04332f
Ricardo Luís Vaz Silva <ricolvs123@gmail.com>
2021-03-24 16:00:44 -0300

    make `build.sh` skip building debug target, only release

--- build.sh
+++ build.sh
@@ -2,21 +2,21 @@
 if command -v RBuild &> /dev/null
 
 then
-    RBuild --file=RBuild.json
+    RBuild --file=RBuild.json release
 else
     
     echo Cannot find RBuild in path, using fallback build commands
     
-    echo Building debug...
+    #echo Building debug...
     
-    mkdir -p build/lin/debug/bin
+    #mkdir -p build/lin/debug/bin
     
-    if g++ src/args.cpp src/json.cpp src/main.cpp src/project.cpp src/targets.cpp src/util.cpp src/drivers.cpp -Iinclude -Werror=return-type -Werror=suggest-override -std=c++20 -fexceptions -g -o build/lin/debug/bin/RBuild
-    then
-        echo Debug build successful
-    else
-        echo Debug build failed
-    fi
+    #if g++ src/args.cpp src/json.cpp src/main.cpp src/project.cpp src/targets.cpp src/util.cpp src/drivers.cpp -Iinclude -Werror=return-type -Werror=suggest-override -std=c++20 -fexceptions -g -o build/lin/debug/bin/RBuild
+    #then
+    #    echo Debug build successful
+    #else
+    #    echo Debug build failed
+    #fi
 
     echo Building release...
     

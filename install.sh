fa18646eb4e12db33f097179d5a83e59b16f06f8
Ricardo Luís Vaz Silva <ricolvs123@gmail.com>
2021-06-15 04:15:16 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 while true; do
     read -p "Install for All users, Local user or Cancel (A/L/C)?" choice
     if [ "$choice" = "a" ] || [ "$choice" = "A" ]; then
-        cp build/lin/release/bin/RBuild /usr/local/bin/RBuild
+        ln -srf build/lin/release/bin/RBuild /usr/local/bin/RBuild
         break;
     elif [ "$choice" = "l" ] || [ "$choice" = "L" ]; then
-        cp build/lin/release/bin/RBuild ~/bin/RBuild;
+        ln -srf build/lin/release/bin/RBuild ~/bin/RBuild;
         break;
     elif [ "$choice" = "c" ] || [ "$choice" = "C" ]; then
         break;
